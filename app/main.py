53f9c809a0c4217726d53da1c8d5d052009368a4
Carlos Isaí Arteaga Trejo <31296448+CR-MX@users.noreply.github.com>
2024-10-17 14:50:13 -0600

    Se diferenció el model under attack

--- app/main.py
+++ app/main.py
@@ -7,55 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 epsilons = [0, .05, .1, .15, .2, .25, .3]
-pretrained_model = "data/lenet_mnist_model.pth"
+pretrained_model = "../data/lenet_mnist_model.pth"
 use_cuda=False
 # Set random seed for reproducibility
 gen = torch.manual_seed(42)
 print(gen)
-
-# LeNet Model definition
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
-
-# MNIST Test dataset and dataloader declaration
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=False, download=True, transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,)),
-            ])),
-        batch_size=1, shuffle=True)
-
-# Define what device we are using
-print("CUDA Available: ",torch.cuda.is_available())
-device = torch.device("cuda" if use_cuda and torch.cuda.is_available() else "cpu")
-
-# Initialize the network
-model = Net().to(device)
-
-# Load the pretrained model
-model.load_state_dict(torch.load(pretrained_model, map_location=device, weights_only=True))
-
-# Set the model in evaluation mode. In this case this is for the Dropout layers
-model.eval()
